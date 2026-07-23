182e2a9cd1bae984421f442b7c18015003615efe
Alex Jones <alexbeckjones@gmail.com>
2026-07-23 09:42:27 +0100

    Add Further details to data and python practice

--- src/main.py
+++ src/main.py
@@ -37,11 +37,38 @@ print(total_tickets_remaining)
 total_sales = (earlybird_tickets_sold * earlybird_ticket_price) + (general_release_tickets_sold * general_release_ticket_price) + (final_release_tickets_sold * final_release_ticket_price)
 print(total_sales)
 
-print("1.4 Basic Maths")
+print("1.4 Basic Maths:")
 earlybird_ticket_revenue = earlybird_tickets_sold * earlybird_ticket_price
 print(earlybird_ticket_revenue)
 general_release_ticket_revenue = general_release_tickets_sold * general_release_ticket_price
 print(general_release_ticket_revenue)
 final_release_ticket_revenue = final_release_tickets_sold * final_release_ticket_price
 print(final_release_ticket_revenue)
+
+print("1.5 Lists:")
+djs_on_the_lineup = ["Alex Jones", "Elev8", "Mack G.", "Reece Ako"]
+print(djs_on_the_lineup)
+first_dj_playing = djs_on_the_lineup[2]
+print(first_dj_playing)
+second_dj_playing = djs_on_the_lineup[0]
+print(second_dj_playing)
+third_dj_playing = djs_on_the_lineup[3]
+print(third_dj_playing)
+fourth_dj_playing = djs_on_the_lineup[1]
+print(fourth_dj_playing)
+future_venues_of_interest = ["Trent Navigation Inn", "Rooftop Social"]
+print(future_venues_of_interest)
+
+print("1.6 Dictionaries:")
+event_details = {
+    "venue": "Bonobo Bar and Canteen",
+    "city": "Chester",
+    "date": "21/8/26"
+}
+print(event_details)
+
+print("1.7 Comments:")
+# Calculate the total revenue from ticket sales
+total_revenue = (earlybird_tickets_sold * earlybird_ticket_price) + (general_release_tickets_sold * general_release_ticket_price) + (final_release_tickets_sold * final_release_ticket_price)
+print("Total revenue from ticket sales: £" + str(total_revenue))
 
